bd4c0fc6bc47c422376552090a79b3fdf1763971
doodung <topofhead@NM-N2103002ui-MacBookPro.local>
2022-10-06 15:50:44 +0900

    react.fc

--- my-app/src/Greetings.tsx
+++ my-app/src/Greetings.tsx
@@ -49,3 +49,9 @@ Greetings.defaultProps = {
 };
 
 export default Greetings;
+
+
+// 정리 : 
+// React.FC 는 별로 좋지 않다.
+// 함수형 컴포넌트를 작성 할 때는 화살표 함수로 작성해도 되고, function 키워드를 사용해도 된다.
+// Props 에 대한 타입을 선언 할 땐 interface 또는 type 을 사용하면 되며, 프로젝트 내부에서 일관성만 지키면 된다.
